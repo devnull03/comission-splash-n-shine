873924b5552bf4fc560539a9d3906a38e457e9de
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-05-04 22:44:31 -0700

    fix: form wasn't loading

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -2,21 +2,30 @@
 	<script src="https://link.msgsndr.com/js/form_embed.js"></script>
 </svelte:head>
 
-<iframe
-	src="https://api.leadconnectorhq.com/widget/form/iqHTKrAbOOGjMJuQrvYW"
-	style="width:100%;height:100%;border:none;border-radius:3px"
-	id="inline-iqHTKrAbOOGjMJuQrvYW"
-	data-layout={"{'id':'INLINE'}"}
-	data-trigger-type="alwaysShow"
-	data-trigger-value=""
-	data-activation-type="alwaysActivated"
-	data-activation-value=""
-	data-deactivation-type="neverDeactivate"
-	data-deactivation-value=""
-	data-form-name="Website Form"
-	data-height="1179"
-	data-layout-iframe-id="inline-iqHTKrAbOOGjMJuQrvYW"
-	data-form-id="iqHTKrAbOOGjMJuQrvYW"
-	title="Website Form"
->
-</iframe>
+<div class="flex h-full w-full flex-col items-center justify-center *:w-full">
+	<p class="text-center text-sm">
+		If the form below doesn't load, please click <a
+			class="italic underline"
+			href="https://api.leadconnectorhq.com/widget/form/iqHTKrAbOOGjMJuQrvYW">here</a
+		>
+	</p>
+
+	<iframe
+		src="https://api.leadconnectorhq.com/widget/form/iqHTKrAbOOGjMJuQrvYW"
+		style="width:100%;height:100%;border:none;border-radius:3px"
+		id="inline-iqHTKrAbOOGjMJuQrvYW"
+		data-layout={"{'id':'INLINE'}"}
+		data-trigger-type="alwaysShow"
+		data-trigger-value=""
+		data-activation-type="alwaysActivated"
+		data-activation-value=""
+		data-deactivation-type="neverDeactivate"
+		data-deactivation-value=""
+		data-form-name="Website Form"
+		data-height="1179"
+		data-layout-iframe-id="inline-iqHTKrAbOOGjMJuQrvYW"
+		data-form-id="iqHTKrAbOOGjMJuQrvYW"
+		title="Website Form"
+	>
+	</iframe>
+</div>
