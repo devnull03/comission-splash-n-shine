b3317839ec7c32394d8d007dd9d182b3c4ee68ce
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-06-10 12:28:06 -0700

    feat: more visible

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -32,7 +32,7 @@
 
 	<div class="aspect-square h-[40vh] overflow-hidden">
 		 <!-- <Logo class="aspect-square h-[40vh] w-auto" /> -->
-		 <Image url="/assets/logo.png" description="company logo" height="200px" width="200px" />
+		 <Image url="/assets/logo.png" description="company logo" height="400px" width="400px" />
 		 <!-- <enhanced:img 
 			src={logoAddr}
 			sizes="400px"
@@ -68,8 +68,8 @@
 		<p>
 			Copyright © 2024 - 2028 {PUBLIC_COMPANY_NAME}. All rights reserved.
 		</p>
-		<p>
-			Website & online presence by <a href="https://rightmarketing.ca" target="_blank" rel="noopener noreferrer" class="underline hover:text-white transition-colors">rightmarketing.ca</a>
+		<p class="text-base text-muted mt-2">
+			Website & online presence by <a href="https://rightmarketing.ca" target="_blank" rel="noopener noreferrer" class="underline hover:text-white transition-colors font-semibold">rightmarketing.ca</a>
 		</p>
 	</div>
 </footer>
